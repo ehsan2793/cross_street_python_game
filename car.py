e4e394f91b8c8207017c5bd37dec12848c3b77f9
ehsan2793 <ehsan2793@gmail.com>
2022-02-25 00:23:37 -0800

    classes are added

---
+++ car.py
@@ -0,0 +1,11 @@
+from turtle import Turtle
+
+COLORS = ['red', 'green', 'yellow', 'blue', 'purple', 'brown', 'orange']
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class Cars(Turtle):
+    def __init__(self):
+        super().__init__()
+        pass
---
+++ scoreBoard.py
@@ -0,0 +1,9 @@
+from turtle import Turtle
+
+FONT = ('Courier', 24, 'normal')
+
+
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        pass
