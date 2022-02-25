4258704605684ae884aec0c28f0422cfb1cc8dd4
ehsan2793 <ehsan2793@gmail.com>
2022-02-25 02:17:28 -0800

    cars are made

--- cars.py
+++ cars.py
@@ -1,3 +1,4 @@
+import random
 from turtle import Turtle
 
 COLORS = ['red', 'green', 'yellow', 'blue', 'purple', 'brown', 'orange']
@@ -5,7 +6,24 @@ STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class Cars(Turtle):
+class Cars:
     def __init__(self):
         super().__init__()
-        pass
+        self.all_cars = []
+
+    def create_new_car(self):
+        rand_number = random.randint(1,8)
+        if rand_number  == 1 :
+
+            new_car = Turtle("square")
+            new_car.penup()
+            new_car.shapesize(stretch_wid=0.6, stretch_len=1.7)
+            new_car.color(random.choice(COLORS))
+            y_position = random.randint(-230, 230)
+            new_car.goto(300, y_position)
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+
+        for car in self.all_cars:
+            car.backward(STARTING_MOVE_DISTANCE)
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import time
 from turtle import Screen
 from player import Player
-
+from cars import Cars
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 screen.bgcolor('black')
 
 player = Player()
+cars = Cars()
+
+
 screen.listen()
 screen.onkey(player.move_left, 'a')
 screen.onkey(player.move_right, 'd')
@@ -18,6 +21,8 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    cars.create_new_car()
+    cars.move_cars()
     # player.move_up()
     # player.move_left()
 
